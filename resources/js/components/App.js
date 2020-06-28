c6e089eb8e3be8df48d495caabfbbcf85981437c
tkrogers3 <tkrogers3@gmail.com>
2020-06-28 18:30:57 -0400

    Task from created

---
+++ resources/js/components/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+ class App extends Component{
+    render(){
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">App Component</div>
+
+                        <div className="card-body">
+                        <form>
+                            <div className="form-group">
+                                <textarea className="form-control" 
+                                rows="5"
+                                 placeholder="Create a new task" 
+                                 required
+                                  />
+                            </div>
+                            <button type="submit" className="btn btn-info">
+                            Create Task
+                            </button>
+
+                        </form>
+                        
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+}
+
+export default App;
