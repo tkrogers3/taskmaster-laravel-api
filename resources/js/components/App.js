4953c6205a0bb795989e8171670edabe32288f13
tkrogers3 <tkrogers3@gmail.com>
2020-07-12 20:10:54 -0400

    Delete functionality on local and backend.

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,86 +1,116 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Axios from 'axios';
 
- class App extends Component{
-     constructor (props){
-         super(props);
-         this.state = {
-             name: '',
-            tasks:[]
-         }
-         this.handleChange = this.handleChange.bind(this);
-         this.handleSubmit = this.handleSubmit.bind(this);
-         this.renderTasks= this.renderTasks.bind(this);    
-         }
-     handleChange(e){
-         this.setState  ({
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            tasks: []
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.renderTasks = this.renderTasks.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    handleChange(e) {
+        this.setState({
             name: e.target.value
-         });
-           
-         }
-// console.log(e.target.value);
-     //handle submit
-handleSubmit(e){
-    e.preventDefault();
-    axios.post('/tasks', {
-        name: this.state.name
-    }).then(response=>{
-       this.setState({
-           tasks: [response.data, ...this.state.tasks],
-           name: ''
-       })
-    });
-}
+        });
 
-renderTasks(){
-    return this.state.tasks.map(task =>(
-        <div key={task.id} className="media">
-        <div className="media-body">
-        <div>
-            {task.name}
-        </div>
-        </div>
-        </div>
-    ) );
-}
+    }
+    // console.log(e.target.value);
+    //handle submit
+    handleSubmit(e) {
+        e.preventDefault();
+        axios.post('/tasks', {
+            name: this.state.name
+        }).then(response => {
+            this.setState({
+                tasks: [response.data, ...this.state.tasks],
+                name: ''
+            })
+        });
+    }
 
-    render(){
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">App Component</div>
+    handleDelete(id){
+        //remove from local state
+        const deleteId = task => task.id !== id;
+        const updatedTasks = this.state.tasks.filter(deleteId);
+        this.setState({tasks: updatedTasks})
 
-                        <div className="card-body">
-                        <form onSubmit={this.handleSubmit}>
-                            <div className="form-group">
-                                <textarea 
-                                onChange={this.handleChange}
-                                className="form-control" 
-                                value = {this.state.name}
-                                maxLength="300"
-                                rows="5"
-                                 placeholder="Create a new task" 
-                                 required
-                                  />
-                                  <hr />
-                                  {this.renderTasks()}
-                            </div>
-                            <button type="submit" className="btn btn-info">
-                            Create Task
+        //Delete request to back end
+axios.delete(`/tasks/${id}`);
+
+
+
+    }
+    renderTasks() {
+        return this.state.tasks.map(task => (
+            <div key={task.id} className="media">
+                <div className="media-body">
+                    <div>
+                        {task.name} {''}
+                        <button
+                         onClick ={() => this.handleDelete(task.id)} className="btn btn-large btn-danger float-right">
+                                        Delete
+                            </button>
+                    </div>
+                </div>
+            </div>
+        ));
+    }
+    //pull tasks from backend via axios
+    getTasks() {
+        axios.get('/tasks').then(response => 
+            this.setState({
+            tasks: [...response.data.tasks]
+        }
+        ))
+    };
+
+    //lifecycle method
+    componentWillMount() {
+        this.getTasks();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">App Component</div>
+
+                            <div className="card-body">
+                                <form onSubmit={this.handleSubmit}>
+                                    <div className="form-group">
+                                        <textarea
+                                            onChange={this.handleChange}
+                                            className="form-control"
+                                            value={this.state.name}
+                                            maxLength="300"
+                                            rows="5"
+                                            placeholder="Create a new task"
+                                            required
+                                        />
+                                        <hr />
+                                        {this.renderTasks()}
+                                    </div>
+                                    <button type="submit" className="btn btn-info">
+                                        Create Task
                             </button>
 
-                        </form>
-                        
-                        
+                                </form>
+
+
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 }
 
 export default App;
